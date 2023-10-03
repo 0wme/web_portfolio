a59ad7f2113e107de3c9bbf659838af4b78e2934
Tom Vieira <skixoxgaming@gmail.com>
2023-10-03 17:01:23 +0200

    Add script.js
    JavaScript

---
+++ script.js
@@ -0,0 +1,26 @@
+
+AOS.init();
+
+
+// Sélectionnez tous les éléments avec la classe "language-image"
+const languageImages = document.querySelectorAll('.language-image');
+
+languageImages.forEach(image => {
+    image.addEventListener('mouseenter', function() {
+        const tooltipId = this.getAttribute('data-tooltip');
+        const tooltip = document.querySelector(`.tooltip[data-tooltip-target="${tooltipId}"]`);
+        if (tooltip) {
+            tooltip.style.display = 'block';
+            tooltip.style.opacity = '1';
+        }
+    });
+
+    image.addEventListener('mouseleave', function() {
+        const tooltipId = this.getAttribute('data-tooltip');
+        const tooltip = document.querySelector(`.tooltip[data-tooltip-target="${tooltipId}"]`);
+        if (tooltip) {
+            tooltip.style.display = 'none';
+            tooltip.style.opacity = '0';
+        }
+    });
+});
