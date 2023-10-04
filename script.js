27b7e7bcc3730613594cb3c1afe26f8a548f9873
Tom Vieira <skixoxgaming@gmail.com>
2023-10-04 09:36:49 +0200

    Add Grid for Tools

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ languageImages.forEach(image => {
 });
 
 
+
+
+const ToolsImages = document.querySelectorAll('.tools-image');
+
+ToolsImages.forEach(image => {
+    image.addEventListener('mouseenter', function() {
+        const tooltipId = this.getAttribute('data-tooltip');
+        const tooltip = document.querySelector(`.tooltip[data-tooltip-target="${tooltipId}"]`);
+        if (tooltip) {
+            tooltip.style.display = 'block';
+            tooltip.style.opacity = '1';
+        }
+    });
+
+    image.addEventListener('mouseleave', function() {
+        const tooltipId = this.getAttribute('data-tooltip');
+        const tooltip = document.querySelector(`.tooltip[data-tooltip-target="${tooltipId}"]`);
+        if (tooltip) {
+            tooltip.style.display = 'none';
+            tooltip.style.opacity = '0';
+        }
+    });
+});
